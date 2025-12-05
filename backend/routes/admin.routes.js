7af88f965de22898ec2f0db1ac5feaf148b7c1f3
Dinesh-raya <dineshraya365@gmail.com>
2025-12-05 09:27:46 +0530

    Remove redundant admin.routes.js - use admin.js only

--- backend/routes/admin.routes.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/admin.routes.js
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/admin.controller');
-const { isAdmin } = require('../middleware/auth');
-
-router.post('/grant-room-access', isAdmin, adminController.grantRoomAccess);
-
-module.exports = router;
